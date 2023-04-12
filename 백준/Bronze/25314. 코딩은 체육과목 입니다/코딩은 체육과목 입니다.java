5dcf49fbf6025d0b2dafbf9548654556974792a3
seoyean <100397088+LIA715@users.noreply.github.com>
2023-04-12 12:58:29 +0900

    Update 코딩은 체육과목 입니다.java

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -2,13 +2,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class joon25314 {
+    public static void main(String[] args) throws IOException
+    //throw~ 필수임. inputStream==null일때 대비해서 예외처리 미리 해줘야함.
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //여기 통해서 N값을 입력 받음
         int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N / 4; i++) {
+        for (int i = 0; i < N / 4; i++)
+        //여기 조건문에서 N/4를 이용해 4의 배수 만큼 반복
+        {
             System.out.print("long ");
         }
-        System.out.println("int");
+        System.out.println("int"); //for문 벗어나면 int 출력
     }
 }
